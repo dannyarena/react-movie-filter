2ef7e875aefeaec063d064208a1a9bbc50ec86f9
dannyarena <arenadanny71@gmail.com>
2025-05-27 16:47:32 +0200

    add array of movies

--- src/App.jsx
+++ src/App.jsx
@@ -1,2 +1,19 @@
 import { useState } from 'react'
 import './App.css'
+
+function App() {
+  const [film, setFilm] = useState([
+  {title: 'Inception', genre: 'Fantascienza'},
+  { title: 'Il Padrino', genre: 'Thriller' },
+  { title: 'Titanic', genre: 'Romantico' },
+  { title: 'Batman', genre: 'Azione' },
+  { title: 'Interstellar', genre: 'Fantascienza' },
+  { title: 'Pulp Fiction', genre: 'Thriller' },
+  ]);
+
+  return (
+   <div></div>
+  )
+}
+
+export default App
